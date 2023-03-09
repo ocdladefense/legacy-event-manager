748217433ee8d31b2b82a3c8e7553cdc8e7bb4e6
Jose Bernal <jbernal.web.dev@gmail.com>
2023-03-09 01:19:47 +0000

    Update js for multiple meals.

--- js/app.js
+++ js/app.js
@@ -21,31 +21,75 @@ globalScripts(["require","libEvent","libData","all/modules/cart/js/cart",
 	  }
 	};
 
-  var listen = function listen(e) {
-  	console.log("change event");
+	var listen = function listen(e) {
+		console.log("change event");
+
+		/*
 		var target = e.target;
 		var data = e.target.dataset || null;
 		if(!target.id) return false;
 		var id = target.id.split('-')[1];
-		domAction("save",id);
-  };	
+		*/
+		domAction("save",e.registrationId);
+	};	
 
    
 	function domAction(action,id){
+		 /*
 		 var domFirst = document.getElementById("firstname-"+id).value;
 		 var domLast = document.getElementById("lastname-"+id).value;
 		 var select = document.getElementById("meal-"+id);
 		 var domMeal = select.options[select.selectedIndex].value;
- 
+		*/
+		let form = new FormData(document.getElementById(id));
+		let obj = {};
+		for (let [key, value] of form) {
+			obj[key] = value;
+		}
+
+		// obj[Meal2Notes__c] = foo;
+		// obj[MealNotes__c] = foo;
+		// obj[Meal2__c] = foo;
+		/*
+		Id: "a2705000000LYOIAA4"
+		firstname: "Jose"
+		lastname: "Bernal"
+		meal-Friday-Lunch: "Turkey"
+		meal-Saturday-Lunch: "none"
+		meal-notes-Friday-Lunch: ""
+		meal-notes-Saturday-Lunch: ""
+		*/
+		console.log(obj);
+		let data = {
+			Id: obj.Id,
+			FirstName__c: obj.FirstName__c,
+			LastName__c: obj.LastName__c,
+			Meal__c: obj['meal-Friday-Lunch'],
+			Meal2__c: obj['meal-Saturday-Lunch'],
+			MealNotes__c: obj['meal-notes-Friday-Lunch'],
+			Meal2Notes__c: obj['meal-notes-Saturday-Lunch']
+		};
+
+		console.log(data);
+		
  		ui.loading();
-		save({ Id: id, FirstName__c: domFirst, LastName__c: domLast, Meal__c: domMeal }).then(function(){ui.status('Saved.');}).then(ui.delay(ui.complete,1000));
+		save(data).then(function(){ui.status('Saved.');}).then(ui.delay(ui.complete,1000));
 	}
    
+   
+	libEvent.domReady(function() {
+		let fn = function(e) {
+			let target = e.currentTarget;
+			let regId = target.dataset && target.dataset.registrationId;
 
-   
-   
-	libEvent.domReady(function(){
-		document.addEventListener("change",listen,true);
+			e.registrationId = regId;
+		};
+
+		document.querySelectorAll(".registration").forEach(function(el) {
+			el.addEventListener("change",fn);
+		});
+
+		document.addEventListener("change",listen);
 		cart.removeAddToCartHandler(); // Remove default cart listeners that may interfere with this app.	
 	});
    
